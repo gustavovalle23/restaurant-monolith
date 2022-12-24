65a233d3c2508d90f3bc5b1a1578194a0f15ac91
gustavovalle23 <gustavo.vallerp@hotmail.com>
2022-12-24 19:22:56 -0300

    remove app files auto generated by nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ReservationModule } from './reservation/reservation.module';
 import { RecipeModule } from './recipe/recipe.module';
 import { OrderModule } from './order/order.module';
 import { AuthModule } from './auth/auth.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
-  imports: [ReservationModule, RecipeModule, OrderModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      include: [AuthModule, OrderModule, RecipeModule, ReservationModule],
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    ReservationModule,
+    RecipeModule,
+    OrderModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
