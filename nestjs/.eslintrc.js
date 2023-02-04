dce505f52881044e49a81870d750fa0e3df19536
Gustavo Valle <gustavo.vallerp@hotmail.com>
2023-02-03 23:41:59 -0300

    update dependencies

--- nestjs/.eslintrc.js
+++ nestjs/.eslintrc.js
@@ -1,24 +1,22 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
+  "env": {
+    "browser": true,
+    "es2021": true
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
+  "overrides": [
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
   },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+  }
+}
