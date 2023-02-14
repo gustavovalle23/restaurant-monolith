9e67e6c980023aceab557b2f0b9b825d35eb5dff
Gustavo Valle <gustavo.vallerp@hotmail.com>
2023-02-13 22:46:27 -0300

    test: adding inicial template for integration tests

---
+++ test/order/integration/order.e2e.spec.ts
@@ -0,0 +1,32 @@
+import { AppModule } from "@/app.module";
+import { INestApplication } from "@nestjs/common"
+import { Test, TestingModule } from "@nestjs/testing";
+import request = require("supertest");
+
+
+describe('Order (e2e) Tests', () => {
+    let app: INestApplication;
+
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule]
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    const gql = "/graphql";
+
+    describe('Create Order', () => {
+        it('Should create an order when correct params', () => {
+            request(app.getHttpServer())
+                .post(gql)
+                .send({})
+        })
+    })
+})
