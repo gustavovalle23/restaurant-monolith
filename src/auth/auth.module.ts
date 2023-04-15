328b87090641949f88c8d5ce48528a61012093c4
Gustavo Valle <gustavo.vallerp@hotmail.com>
2023-04-14 21:58:53 -0300

    delete useless files

--- src/auth/auth.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthResolver } from './auth.resolver';
-
-@Module({
-  providers: [AuthResolver, AuthService],
-})
-export class AuthModule {}
