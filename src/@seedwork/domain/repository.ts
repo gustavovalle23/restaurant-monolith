125d29312df8f7f9d3a1666caa3240a32b6b8722
Gustavo Valle <gustavo.vallerp@hotmail.com>
2023-04-14 22:08:34 -0300

    removing seedwork

--- src/@seedwork/domain/repository.ts
+++
@@ -1,8 +0,0 @@
-export interface Repository<Entity, Identifier> {
-    create(input: any): Entity | Promise<Entity>
-    findById(id: Identifier): Entity | Promise<Entity>
-    findAll(pagination: Pagination): Entity[] | Promise<Entity[]>
-    remove(id: Identifier): Promise<void>;
-}
-
-type Pagination = { limit: number, skip: number }
--- src/@seedwork/usecase/use-case.ts
+++
@@ -1,3 +0,0 @@
-export interface UseCase<Input, Output> {
-    execute(input: Input): Output | Promise<Output>
-}
